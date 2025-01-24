fda56330de8e0d9bb6be86aeaf36c9171a53d9d3
Juan Guerra <juan.l.guerrar@gmail.com>
2025-01-24 16:38:14 -0500

    Pantalla de inicio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Tiempo en milisegundos para mostrar la pantalla inicial
+setTimeout(() => {
+    window.location.href = "login.html"; // Cambia a la página de inicio de sesión
+  }, 3000); // 3 segundos
+  
