38896361d45d38c696608a1dc31d3cbedef1b815
Juan Guerra <juan.l.guerrar@gmail.com>
2025-01-25 15:15:55 -0500

    Primer despliege de los entrenamientos

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function irHomePrincipal(){
   window.location.href = "home-principal.html"
 }
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('card-expandida');
+  });
+});
+
+
+const btnDia = document.getElementById('btn-dia');
+const elemento = document.getElementById('ejercicio');
+
+btnDia.addEventListener('click', () => {
+   // Comprobamos si la clase 'visible' está presente
+  if(elemento.classList.contains('visible')){
+     // Si está presente, la removemos
+    elemento.classList.remove('visible');
+  } else{
+    elemento.classList.add('visible')
+  }
+  
+});
